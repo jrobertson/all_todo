d0e097f2956701723d02d18dfa0f56ddabc4f013
Your Name <you@example.com>
2016-11-21 11:11:14 +0000

    Todo items can now be broken down into further detail which includes the fields *when, duration, and priority*.

--- all_todo.gemspec
+++ all_todo.gemspec
@@ -1,9 +1,10 @@
 Gem::Specification.new do |s|
   s.name = 'all_todo'
-  s.version = '0.1.1'
+  s.version = '0.2.0'
   s.summary = 'Reads a plain text file called all_todo.txt and generates a Polyrex document from it and more.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/all_todo.rb']
+  s.add_runtime_dependency('pxrowx', '~> 0.1', '>=0.1.0')
   s.add_runtime_dependency('polyrex-headings', '~> 0.1', '>=0.1.4')
   s.signing_key = '../privatekeys/all_todo.pem'
   s.cert_chain  = ['gem-public_cert.pem']
--- lib/all_todo.rb
+++ lib/all_todo.rb
@@ -3,6 +3,7 @@
 # file: all_todo.rb
 
 
+require 'pxrowx'
 require 'polyrex-headings'
 
 
@@ -14,12 +15,80 @@ class AllTodo
     lines = s.lines
     lines.shift 3
     
-    declaration = '<?ph schema="sections[title]/section[x]"?>'
-    @px = PolyrexHeadings.new(declaration + "\n" + lines.join).to_polyrex
+    @fields = %w(todo when duration priority)
+    declar = "<?ph schema='sections[title]/section[#{@fields.join(',')}]'" + 
+        " format_masks[0]='[!todo]'?>"
+
+    @px = PolyrexHeadings.new(declar + "\n" + lines.join).to_polyrex
+
+
+  end
+  
+  def detail()
+    
+    doc = Rexle.new(@px.to_xml)
+    a = scan doc.root.element('records')
+    lines = scan_print a
+    
+    filename = 'all_todo_detail.txt'
+    heading = [filename, '=' * filename.length, '', ''].join("\n")
+    
+    heading + lines.join("\n")    
+    
+  end
+  
+  alias breakdown detail
+  
+  def parse_detail(s)
+    
+    lines = s.lines
+    lines.shift 3
+    pr = PxRowX.new(lines.join)
+    pr.to_xml pretty: true    
 
   end
 
   def to_px
     @px
   end
+  
+  private
+  
+  
+  def scan(node)
+
+    node.elements.map do |section|
+
+      summary = section.xpath('summary').map do |x|
+        @fields.map {|y| x.text(y)}
+      end
+
+      summary + scan(section.element('records'))
+    end
+  end
+
+  def scan_print(a, indent=0)
+
+    lines = []
+
+    a.each do |row_children|
+
+      row = row_children.shift
+      
+      @fields.zip(row).each do |col|
+
+        label, value = col
+        lines << '  ' * indent + [label, value].join(': ')
+        
+      end
+
+      lines << "\n"
+
+      lines.concat scan_print(row_children, indent+1) if row_children.any?
+
+    end
+
+    lines
+  end
+  
 end
